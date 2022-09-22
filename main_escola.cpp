5edf67827b7caaa6895c38e5e707090dd6914454
zorin375 <vitormanoelbasilio357@gmail.com>
2022-09-22 17:07:22 -0300

    adicionando função ao codigo

--- main_escola.cpp
+++ main_escola.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
- /* estou dando inicio a um novo desafio. nesse novo projeto eu irei criar uma nova versão 
+/* estou dando inicio a um novo desafio. nesse novo projeto eu irei criar uma nova versão
 do desafio git "escola mãos que movem o amanhã". o codigo sera inteiramente baseado em sua versão inicial escrita
 em portugol studio.  */
 
@@ -11,89 +11,102 @@ em portugol studio.  */
 /*$$$$$$$$$$$$$$$*/
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int calculo_media () {
+	float med[4];
+	float media, soma = 0;
+	int contador;
+
+	for (contador =0; contador<= 3; contador++) {
+		printf ("\ninforme a %da. nota :", contador + 1);
+		scanf ("%a", &med[contador]);
+		soma += med[contador];
+	}
+	media = soma/4;
+	printf ("\na nota final do aluno vale a: %6.2f\n\n", media);
+}
 
 int main(int argc, char** argv) {
-//variaveis 
-int nota1,nota2,media,mediaf;
-int opcao,login,senha,turma;
-char professor[30]= ("vitor");
-char aluno [30]= ("caio");
-char nome [30];	
+//variaveis
+	int opcao,login,senha,turma;
+	char professor[30]= ("vitor");
+	char aluno [30]= ("caio");
+	char nome [30];
 // variaveis
 
 // iremos tentar algo novo com uma estrutura de repeticao
 
-do{
-	printf ("****************seja bem vindo a escola maos que movem o manha****************");
-	printf ("\n\nescolha uma opcao");
-	printf ("\n1 se sou aluno\n2 se sou professor\n");
-	scanf ("%d",&opcao);
-}  while ((opcao<1) && (opcao>2));
+	do {
+		printf ("****************seja bem vindo a escola maos que movem o manha****************");
+		printf ("\n\nescolha uma opcao");
+		printf ("\n1 se sou aluno\n2 se sou professor\n");
+		scanf ("%d",&opcao);
+	}  while ((opcao<1) && (opcao>2));
 
 // aqui entraremos com os outros dados usando switch case
 
-switch (opcao) {
-	// login e senha do aluno
-	case 1:
-	printf ("\nola, seja bem vindo a nossa escola");
-	printf ("\ninsira seu login:");
-	scanf ("%d",&login);
-	printf ("\ninsira sua senha:");
-	scanf ("%d", &senha);
-    if ((login==44) && (senha==333)){	
-    	printf ("\nvamos la %s",aluno);
-    	printf ("\nsua media foi 10");
-	} 
-	break; 
-	// login e senha do professor
-	case 2:
-	printf ("\ninsira seu login e senha");
-	printf ("\ninsira seu login:");
-	scanf ("%d",&login);
-	printf ("\ninsira sua senha:");
-	scanf ("%d", &senha);
-    if ((login==22) && (senha==2002)){	
-    	printf ("\nvamos la professor %s",professor);
-    	printf ("\nescolha sua opcao");
-    	printf ("\n1 para calcular media\n2 para cadastrar um novo aluno\n");
-    	scanf ("%d",&opcao);
-    	switch (opcao) {
-    		// aqui calculamos a media 
-    		case 1:
-    			printf ("\nentre com as notas");
-    			printf ("\nnota 1:");
-    			scanf ("%d",&nota1);
-    			printf ("\nnota 2:");
-    			scanf ("%d",&nota2);
-    			media = (nota1 + nota2); 
-    			mediaf = media/2;
-    			if (mediaf >= 7) {
-    				printf("\nMedia do aluno = %d ",mediaf);
-    				printf ("\no aluno esta aprovado");
-				} else {
-					printf("\nMedia do aluno = %d ",mediaf);
-					printf ("\no aluno esta reprovado");
+	switch (opcao) {
+			// login e senha do aluno
+		case 1:
+			printf ("\nola, seja bem vindo a nossa escola");
+			printf ("\ninsira seu login:");
+			scanf ("%d",&login);
+			printf ("\ninsira sua senha:");
+			scanf ("%d", &senha);
+			if ((login==44) && (senha==333)) {
+				printf ("\nvamos la %s",aluno);
+				printf ("\ndeseja calcular sua media ?");
+				printf ("\n1 se sim \n2 se nao\n");
+				scanf ("%d", &opcao);
+				switch (opcao) {
+					case 1:
+						// função media
+						int calculo_media();
+						calculo_media();
+						break;
+
+					case 2:
+						printf ("\nok!!!");
+						break;
 				}
-    			
-    		break; 
-			// aqui nos cadastramos um novo aluno
-			case 2:
-				printf ("\ninsira os dados do aluno\n");
-				printf ("nome:");
-				scanf ("%s", &nome);
-				printf ("\nturma:");
-				scanf ("%d", &turma);
-				printf ("o'a aluno'a %s",nome);
-				printf (" faz parte da turma ");
-				printf ("%d",turma);
-			break; 	
-		}
-	} 
-	break;
-	
-}    /*tive algumas dificuldades em relação a grande diferença entre a sintax do portugol e da linguagem c. 
-       A logica tambem tem uma grande diferença, porem com a tentativa e erro e com a ajuda dos material da 
+			}
+			break;
+			// login e senha do professor
+		case 2:
+			printf ("\ninsira seu login e senha");
+			printf ("\ninsira seu login:");
+			scanf ("%d",&login);
+			printf ("\ninsira sua senha:");
+			scanf ("%d", &senha);
+			if ((login==22) && (senha==2002)) {
+				printf ("\nvamos la professor %s",professor);
+				printf ("\nescolha sua opcao");
+				printf ("\n1 para calcular media\n2 para cadastrar um novo aluno\n");
+				scanf ("%d",&opcao);
+				switch (opcao) {
+						// aqui calculamos a media
+					case 1:
+						// função media
+						int calculo_media();
+						calculo_media();
+						break;
+						// aqui nos cadastramos um novo aluno
+					case 2:
+						printf ("\ninsira os dados do aluno\n");
+						printf ("nome:");
+						scanf ("%s", &nome);
+						printf ("\nturma:");
+						scanf ("%d", &turma);
+						printf ("o'a aluno'a %s",nome);
+						printf (" faz parte da turma ");
+						printf ("%d",turma);
+						break;
+				}
+			}
+			break;
+
+	}    /*tive algumas dificuldades em relação a grande diferença entre a sintax do portugol e da linguagem c.
+       A logica tambem tem uma grande diferença, porem com a tentativa e erro e com a ajuda dos material da
 	   faculdade, da internet e dos meus livros fui capaz de superar as dificuldades.**************/
-	
+
 	return 0;
 }
