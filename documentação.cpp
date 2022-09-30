f9654a1ee0d6c8c1e952a899dc4e284422ded06a
zorin375 <vitormanoelbasilio357@gmail.com>
2022-09-30 04:32:10 -0300

    refatoração do codigo e uma nova estrutura de retorno

---
+++ documentação.cpp
@@ -0,0 +1,9 @@
+/*esta foi uma breve revisão da nova versão do programa desafio git escola mãos que movem o amanhã*/
+
+// este codigo foi inteiramente escrito em c++
+
+// conta com uma estrutura de repetição responsavel por dar retorno ao inicio do programa
+
+//conta com uma função para calcular a media em dois pontos diferentes do programa 
+
+// autor : vitorozorio !!!!
--- main_escola.cpp
+++ main_escola.cpp
@@ -11,42 +11,50 @@ em portugol studio.  */
 /*$$$$$$$$$$$$$$$*/
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-float calculo_media (float *media) {
-	float nota1, nota2, nota3, nota4;
-        printf ("\ninsira as notas ");
-        printf ("\nnota1 :");
-        scanf ("%f", &nota1 );
-        printf ("\nnota2 :" );
-        scanf ("%f", &nota2 );
-        printf ("\nnota3 :" );
-        scanf ("%f", &nota3 );
-        printf ("\nnota4 :" );
-        scanf ("%f", &nota4 );
-        media = ( nota1 + nota2 + nota3 + nota4 ) / 4;
-        printf ( "\nmedia final :%.2f", media );
-        return media; 
+float calculo_media () {
+	float nota1, nota2, nota3, nota4, media;
+	printf ("\n--------------------------------\n");
+	printf ("insira as notas !!");
+	printf ("\nnota1 :");
+	scanf ("%f", &nota1 );
+	printf ("\nnota2 :");
+	scanf ("%f", &nota2 );
+	printf ("\nnota3 :");
+	scanf ("%f", &nota3 );
+	printf ("\nnota4 :");
+	scanf ("%f", &nota4 );
+	media = (nota1 + nota2 + nota3 + nota4) / 4;
+	printf ("\nmedia final do aluno :%.2f", media );	
+	printf ("\n--------------------------------\n");
+	return media ;
 }
 int main(int argc, char** argv) {
 //variaveis
 	int opcao, login, senha, turma;
-	char professor[30]= ("vitor");
-	char aluno [30]= ("caio");
+	char professor[30] = ("vitor");
+	char aluno [30] = ("caio");
 	char nome [30];
+	bool verificador = false ;
 // variaveis
+
+do {
+// inicio 	    
 		printf ("****************seja bem vindo a escola maos que movem o manha****************");
 		printf ("\n\nescolha uma opcao");
 		printf ("\n1 se sou aluno\n2 se sou professor\n");
 		scanf ("%d",&opcao);
-// aqui entraremos com os outros dados usando switch case
-	switch (opcao) {
-			// login e senha do aluno
+     // aqui entraremos com os outros dados usando switch case
+	 switch (opcao) {
+        // login e senha do aluno
 		case 1:
+			printf ("\n--------------------------------\n");
 			printf ("\nola, seja bem vindo a nossa escola");
 			printf ("\ninsira seu login:");
 			scanf ("%d", &login);
 			printf ("\ninsira sua senha:");
 			scanf ("%d", &senha);
-			if ((login==44) && (senha==333)) {
+			if ((login == 44) && (senha == 333)) {
+				printf ("\n--------------------------------\n");
 				printf ("\nvamos la %s",aluno);
 				printf ("\ndeseja calcular sua media ?");
 				printf ("\n1 se sim \n2 se nao\n");
@@ -54,8 +62,7 @@ int main(int argc, char** argv) {
 				switch (opcao) {
 					case 1:
 					// função media
-					//float calculo_media();
-					calculo_media(media);
+					calculo_media();
 					break;
                     // fim do programa
 					case 2:
@@ -64,17 +71,20 @@ int main(int argc, char** argv) {
 				}
 			} 
 			else {
-				printf ("\n ERRO!!!");
+				printf ("\n--------------------------------\n");
+				printf ("ERRO!!!");
 			}
-		break;
+		 break;
 			// login e senha do professor
-		case 2:
-			printf ("\ninsira seu login e senha");
-			printf ("\ninsira seu login:");
-			scanf ("%d",&login);
-			printf ("\ninsira sua senha:");
-			scanf ("%d", &senha);
-			if ((login==22) && (senha==2002)) {
+		 case 2:
+			 printf ("\n--------------------------------\n");
+			 printf ("\ninsira seu login e senha");
+			 printf ("\ninsira seu login:");
+			 scanf ("%d",&login);
+			 printf ("\ninsira sua senha:");
+			 scanf ("%d", &senha);
+			 if ((login == 22) && (senha == 2002)) {
+				printf ("\n--------------------------------\n");
 				printf ("\nvamos la professor %s",professor);
 				printf ("\nescolha sua opcao");
 				printf ("\n1 para calcular media\n2 para cadastrar um novo aluno\n");
@@ -83,11 +93,11 @@ int main(int argc, char** argv) {
 				// aqui calculamos a media
 				case 1:
 				// função media
-				//float calculo_media();
-				calculo_media(media);
+				calculo_media();
 				break;
 				// aqui nos cadastramos um novo aluno
 			    case 2:
+			    printf ("\n--------------------------------\n");	
 				printf ("\ninsira os dados do aluno\n");
 				printf ("nome:");
 				scanf ("%s", &nome);
@@ -98,15 +108,30 @@ int main(int argc, char** argv) {
 				printf ("%d",turma);
 				break;
 				}
-			}
-			else {
-		        printf ("\n ERRO!!!");
-			}
-		break;
-
-	}    /*tive algumas dificuldades em relação a grande diferença entre a sintax do portugol e da linguagem c.
-       A logica tambem tem uma grande diferença, porem com a tentativa e erro e com a ajuda dos material da
-	   faculdade, da internet e dos meus livros fui capaz de superar as dificuldades.**************/
+			 }
+			 else {
+				printf ("\n--------------------------------\n");
+		        printf ("ERRO!!!");
+			 }
+		 break;	  
+		 }  
+	      printf ("\n--------------------------------\n");
+	      printf ("\ndeseja voltar 0 \ndeseja encerrar 1\n");
+   	      printf ("\nescolha uma opcao :");
+   	      scanf ("%d", &opcao);
+   	      switch (opcao) {
+   	      	
+   		  case 0 :
+   		  verificador = true;	
+   		  break;	
+   		
+   		  case 1 :
+   		  verificador = false;
+		  printf ("\nprograma finalizado !!");   	
+   		  break;	
+	   }
+   	}
+ while (verificador);
 
 	return 0;
 }
